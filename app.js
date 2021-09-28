a63e7f0f7a49cdefaa93918e034b08fb694450d0
Tomytool <56263086+Tomytool@users.noreply.github.com>
2021-09-28 12:33:15 -0300

    se crea app de calculo de area

---
+++ app.js
@@ -0,0 +1,7 @@
+// area de un cuadrado
+
+function mi_funcion() {
+  let lado = parseInt(prompt('Ingrese el valor de A en cm: '));
+  let resultado = lado * lado;
+  alert('el valor de area de un cuadrado es: ' + resultado + ' cm2');
+}
